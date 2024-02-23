3000d007b29855746ffc486fccbcdd4c30531388
lazermagma <masterabdelrahman7@gmail.com>
2024-02-23 17:50:25 +0200

    ADD: Search filter

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+
+// import { filterData, getFilterValues } from "../utils/filterData";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
+import React from "react";
+
+const SearchFilters = () => {
+  return <div>SearchFilters</div>;
+};
+
+export default SearchFilters;
