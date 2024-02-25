c8f2951e73f6a23ecb8885e5e165d9e7c77aeacb
lazermagma <masterabdelrahman7@gmail.com>
2024-02-25 23:35:25 +0200

    Delete unused files and update tailwind.config.js

---
+++ pages/search.js
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Flex, Box, Text, Icon } from "@chakra-ui/react";
+import { BsFilter } from "react-icons/bs";
+import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from "../assets/images/noresult.svg";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+const search = ({ properties }) => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box>
+      <Flex
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
+        p="2"
+        fontWeight="black"
+        fontSize="lg"
+        justifyContent="center"
+        alignItems="center"
+        onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
+      >
+        <Text>Search Property By Filters</Text>
+        <Icon paddingleft="2" w="7" as={BsFilter} />
+      </Flex>
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query?.purpose}
+      </Text>
+      <Flex flexWrap="wrap">
+        {properties?.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      {properties?.length === 0 && (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          marginTop="5"
+          marginBottom="5"
+        >
+          <Image alt="no result" src={noresult} />
+          <Text fontSize="2xl" marginTop="3">
+            No Results Fouproperties
+          </Text>
+        </Flex>
+      )}
+    </Box>
+  );
+};
+
+//fetching data from the api
+export async function getStaticProps() {
+  const propertiesForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertiesForSale?.hits,
+    },
+  };
+}
+
+export default search;
